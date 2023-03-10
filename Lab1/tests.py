37a62673a677f431b9a065edf33e45f03d317b19
jkwiatk1 <01142696@pw.edu.pl>
2023-03-10 03:46:55 +0100

    Add experiment with time counting

--- Lab1/tests.py
+++ Lab1/tests.py
@@ -1,25 +1,100 @@
+import random
+import time
+import gc
 import numpy as np
+import matplotlib.pyplot as plt
 from SteepestGradientDescent import stochastic_gradient_descent
 from NewtonMethod import newtons_method
 
-x_min, y_min, f_min ,iter_num= None, None, float('inf'), 0
+workTimes_SGD = []
+workIters_SGD = []
 
-for i in range(10):
-    x, y, f_val, iter_num = stochastic_gradient_descent(0.01, 1000)
-    if f_val < f_min:
-        x_min, y_min, f_min = x, y, f_val
+workTimes_NM = []
+workIters_NM = []
 
-print("Minimum value of f(x,y) = (x+2*y-7)^2 + (2*x+y-5)^2 is {:.2f} at x = {:.2f} and y = {:.2f}".format(f_min, x_min, y_min))
-print(iter_num)
+experimentSize = 25
+Beta = 0.01
+MaxIter = 1000
+Eps = 1e-5
+
+def SGD_timeCount(x0,y0):
+    gc_old = gc.isenabled()  # pobierz aktualny stan odśmiecania
+    gc.disable()  # wyłącz odśmiecanie
+    start = time.process_time()  # pobierz aktualny czas
+    xminSGD, yminSGD, fminSGD, iterNumSGD = stochastic_gradient_descent(x0, y0, Beta, MaxIter, Eps)
+    stop = time.process_time()
+    time_list = stop-start
+    if gc_old: gc.enable() # przywróć pierwotny stan odśmiecania
+    return xminSGD, yminSGD, fminSGD, iterNumSGD, time_list
+
+def SGD_timeCount(x0, y0):
+    gc_old = gc.isenabled()  # pobierz aktualny stan odśmiecania
+    gc.disable()  # wyłącz odśmiecanie
+    start = time.process_time()  # pobierz aktualny czas
+    xminNM, yminNM, fminNM, iterNumNM = newtons_method(x0, y0, Beta, MaxIter, Eps)
+    stop = time.process_time()
+    time_list = stop-start
+    if gc_old: gc.enable() # przywróć pierwotny stan odśmiecania
+    return xminNM, yminNM, fminNM, iterNumNM, time_list
+
+for i in range(experimentSize):
+    x0 = random.uniform(-5, 5)
+    y0 = random.uniform(-5, 5)
+    xminSGD, yminSGD, fminSGD, iterNumSGD, timeSGD = SGD_timeCount(x0, y0)
+    xminNM, yminNM, fminNM, iterNumNM, timeNM = SGD_timeCount(x0, y0)
+    drawnPointsSGD = np.array([x0, y0])
+    bestPointSGD = np.array([xminSGD, yminSGD])
+    bestPoinsNM = np.array([xminNM, yminNM])
+    workTimes_SGD.append(timeSGD)
+    workIters_SGD.append(iterNumSGD)
+    workTimes_NM.append(timeNM)
+    workIters_NM.append(iterNumNM)
+    print("------------------------------------------------------------------")
+    print("TEST nb: ", i, end = " ")
+    print("and starting points [x,y] = ", drawnPointsSGD)
+    print()
+    print("Stochastic gradient descent")
+    print("Minimum at: ", bestPointSGD)
+    print("Minimum value =", fminSGD)
+    print("Number of iterations =", iterNumSGD)
+    print("Counting time: ", timeSGD)
+    print()
+    print("Newton's Method")
+    print("Minimum at: ", bestPoinsNM)
+    print("Minimum value =", fminNM)
+    print("Number of iterations =", iterNumNM)
+    print("Counting time: ", timeNM)
+    print()
+    print()
+
+plt.plot(range(experimentSize), workTimes_SGD,'*-r', label = "Stochastic gradient descent")
+plt.plot(range(experimentSize), workTimes_NM,'*-g', label = "Newton's method")
+plt.legend(loc="upper left")
+
+plt.xlabel('Experiment number')
+plt.ylabel('Time [s]')
+plt.title('Working time of each method')
+
+# Show the plot
+plt.show()
+
+plt.plot(range(experimentSize), workIters_SGD,'*-r', label = "Stochastic gradient descent")
+plt.plot(range(experimentSize), workIters_NM,'*-g', label = "Newton's method")
+plt.legend(loc="upper left")
+
+plt.xlabel('Experiment number')
+plt.ylabel('Number of iterations ')
+plt.title('Number of iterantions for each method')
+
+# Show the plot
+plt.show()
 
 
-
-
-iter_num = 0
-f_minNewton = 0
-x0 = np.array([-4.1221, 3.2131412], dtype=float)
-xmin,ymin, f_minNewton, iter_num = newtons_method(x0[0],x0[1])
-print(iter_num)
-print("Minimum at x =", xmin)
-print("Minimum at y =", ymin)
-print("Minimum value =", f_minNewton)
+# Add axis labels and a title
+plt.hist(workTimes_SGD, bins=experimentSize, alpha=0.5, label="Stochastic gradient descent")
+plt.hist(workTimes_NM, bins=experimentSize, alpha=0.5, label="Newton's method")
+plt.legend(loc="upper left")
+plt.xlabel('Working time of each method')
+plt.ylabel('Frequency')
+plt.title('Histogram')
+plt.show()
