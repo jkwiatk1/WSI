17858bc039da875f40ebe40ac5b5ec8552cb2402
jkwiatk1 <01142696@pw.edu.pl>
2023-03-24 00:49:35 +0100

    Modyfikacja zwracanego najlepszego wektora

--- Lab2/src/geneticAlgorithm.py
+++ Lab2/src/geneticAlgorithm.py
@@ -5,7 +5,7 @@ import numpy as np
 from initialPopulationGenerator import generate_population
 
 
-# random.seed(123)
+random.seed(123)
 
 class GeneticAlgorithm:
     def __init__(self, edges, nodes,  num_generations, mutation_probability, population_size, tournament_size, selection_size, mutation_rate):
@@ -18,16 +18,37 @@ class GeneticAlgorithm:
         self.selection_size = selection_size
         self.mutation_rate = mutation_rate
 
+    # def calculate_fitness(self,individual, edges):
+    #     fitness = sum(1 for edge in edges if individual[edge[0]] or individual[edge[1]])
+    #     return fitness
+    #
+    # def calculate_fitness_for_all(self, population):
+    #     fitness_for_all = []
+    #     for individual in population:
+    #         fitness = self.calculate_fitness(individual, self.edges)
+    #         fitness_for_all.append(fitness)
+    #     return  fitness_for_all
+
+    def calculate_penalty(self,individual):
+        num_ones = sum(individual)
+        penalty = num_ones * 0.5
+        return penalty
+
     def calculate_fitness(self,individual, edges):
         fitness = sum(1 for edge in edges if individual[edge[0]] or individual[edge[1]])
+
+        penalty = self.calculate_penalty(individual)
+        fitness -= penalty
+
         return fitness
 
-    def calculate_fitness_for_all(self, population):
+    def calculate_fitness_for_all(self,population):
         fitness_for_all = []
         for individual in population:
             fitness = self.calculate_fitness(individual, self.edges)
             fitness_for_all.append(fitness)
-        return  fitness_for_all
+        return fitness_for_all
+
 
 
     def tournament_selection(self,population, fitness_scores, tournament_size, selection_size):
@@ -44,7 +65,7 @@ class GeneticAlgorithm:
         individual_mutated = individual.copy()
         num_mutations = int(np.random.normal(loc=mutation_rate * len(individual), scale=1))
         for i in range(num_mutations):
-            index_to_mutate = random.randint(0, len(individual))
+            index_to_mutate = random.randint(0, len(individual)-1)
             individual_mutated[index_to_mutate] = 1 - individual[index_to_mutate]
         return individual_mutated
 
@@ -69,16 +90,16 @@ class GeneticAlgorithm:
             fitness_scores = self.calculate_fitness_for_all(Tt)
 
             elite_indices = sorted(range(len(Tt)), key=lambda i: fitness_scores[i], reverse=True)[:len(population)]
+            #elite_indices = sorted(range(len(Tt)), key=lambda i: (fitness_scores[i], -sum(Tt[i])), reverse=True)[:len(population)]
             population = [Tt[i] for i in elite_indices]
 
-        print(population)
         print(fitness_scores)
+        print(population)
 
         # # Wybór najlepszego osobnika
+        # best_individual = max(population, key=lambda individual: self.calculate_fitness(individual, self.edges))
+        # best_individual = max(population, key=lambda individual: (self.calculate_fitness(individual, self.edges), -sum(individual)))
         best_individual = max(population, key=lambda individual: self.calculate_fitness(individual, self.edges))
+        best_individual = min(filter(lambda individual: individual == best_individual,population), key=lambda individual: sum(individual))
         return best_individual, self.calculate_fitness(best_individual, self.edges)
-
-
-
-
 
