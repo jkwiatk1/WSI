8c1bdb2cc842fcd20c3f703fe8fa26c853a123e8
jkwiatk1 <01142696@pw.edu.pl>
2023-03-24 11:12:42 +0100

    Modyfikacje

--- Lab2/src/experiments.py
+++ Lab2/src/experiments.py
@@ -1,30 +1,44 @@
 # author: Jan Kwiatkowski
 
+import numpy as np
 from graphsGenerator import create_complete_graph, create_bipartite_graph, create_random_graph
-from graphVisualizer import show_graph
 from geneticAlgorithm import GeneticAlgorithm
 
+numberOfIteration = [10,20,30,40,50,60,70,80,90,100,500]
 
 """
 Graf pełny
 """
-complete_graph_1_nodes, complete_graph_1_edges  = create_complete_graph(25)
-number_of_all_edges_Complete = len(complete_graph_1_edges)
-
-
-def expetiment_for_CompleteGraph(num_of_experiment,num_generations):
+def expetiment_for_CompleteGraph(num_of_experiment,num_generations,population_size,selection_size):
+    complete_graph_1_nodes, complete_graph_1_edges = create_complete_graph(25)
+    number_of_all_edges_Complete = len(complete_graph_1_edges)
     result_for_CompleteGraph = []
+    result_cover = []
     for i in range(num_of_experiment):
         GeneticAlgorithmInit_Complete = GeneticAlgorithm(edges=complete_graph_1_edges, nodes=complete_graph_1_nodes,
-                                                         num_generations=num_generations, mutation_probability=0.01, population_size=50,
-                                                         tournament_size=2, selection_size=80, mutation_rate=0.01)
+                                                         num_generations=num_generations, mutation_probability=0.01, population_size=population_size,
+                                                         tournament_size=2, selection_size=selection_size, mutation_rate=0.01)
         best = GeneticAlgorithmInit_Complete.genetic_algorithm()
-        result_cover = best[1]/number_of_all_edges_Complete
+        result_cover.append(best[1]/number_of_all_edges_Complete)
         result_for_CompleteGraph.append([best[1],number_of_all_edges_Complete,result_cover])
-    return result_for_CompleteGraph
+    return result_for_CompleteGraph, result_cover
+
+def exp_CG():
+    for num in numberOfIteration:
+        CG_exp1_all, CG_exp1_cover = expetiment_for_CompleteGraph(num_of_experiment = 51,num_generations = num,population_size = 50,selection_size = 80)
+        std_dev = np.std(CG_exp1_cover)
+        min_CG_cover = min(CG_exp1_cover)
+        max_CG_cover = max(CG_exp1_cover)
+        mean_CG_cover = np.mean(CG_exp1_cover)
+        print(f"Obliczenia dla: {num} iteracji", )
+        print("Pokrycie: ", CG_exp1_cover)
+        print("Odchylenie standardowe: ", std_dev)
+        print(f"Min: {min_CG_cover}, Max: {max_CG_cover}, Avg: {mean_CG_cover}")
+        # print("Wyniki: ", CG_exp1_all)
+        print()
 
 
-print(expetiment_for_CompleteGraph(5,500))
+exp_CG()
 
 """
 Graf dwudzielny
