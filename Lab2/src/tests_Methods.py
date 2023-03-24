2a9acb7bb8ab1bced94499ef00a5042a3a3db1d0
jkwiatk1 <01142696@pw.edu.pl>
2023-03-24 08:24:04 +0100

    Test metod

--- Lab2/src/tests_Methods.py
+++ Lab2/src/tests_Methods.py
@@ -6,30 +6,35 @@ import numpy as np
 """
 Test metody która ma za zadanie mutować osobnika
 """
+print("MUTACJA")
 def mutation(individual, mutation_rate):
-    individual_mutated = individual.copy()
+    individual_m = individual.copy()
     num_mutations = int(np.random.normal(loc=mutation_rate * len(individual), scale=1))
     for i in range(num_mutations):
-        index_to_mutate = random.randint(0, len(individual))
-        individual_mutated[index_to_mutate] = 1 - individual[index_to_mutate]
-    return individual_mutated
+        index_to_mutate = random.randint(0, len(individual) - 1)
+        individual_m[index_to_mutate] = 1 - individual[index_to_mutate]
+    return individual_m
 
 
 vector = [1, 0, 1, 1, 1, 1, 0, 1]
 mutation_rate = 0.1
 mutated_vector = mutation(vector, mutation_rate)
 
-print("Original vector:", vector)
-print("Mutated vector:", mutated_vector)
+print("Oryginalny wektor:", vector)
+print("Zmutowany wektor:", mutated_vector)
+print()
+
 
 
 """
 Test metody do oceny populacji
 """
+print("OCENA POPULACJI")
 def calculate_fitness(individual, edges):
-    fitness = sum(1 for edge in edges if individual[edge[0]] or individual[edge[1]])
+    cost = sum(individual)  ** 2
+    covered_edges = sum(1 for edge in edges if individual[edge[0]] or individual[edge[1]])
+    fitness = -cost + covered_edges
     return fitness
-
 
 def calculate_fitness_for_all(population, edges):
     fitness_for_all = []
@@ -40,16 +45,18 @@ def calculate_fitness_for_all(population, edges):
 
 
 edges = [(0, 1), (1, 2), (2, 3), (3, 0)]
-population = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 1, 0, 0]]
-
+population = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 1, 0, 0], [1, 1, 0, 1], [1, 0, 0, 1], [0, 0, 0, 1], [1, 1, 1, 0]]
+print("Populacja: ", population)
 fitness_for_all = calculate_fitness_for_all(population, edges)
-print(fitness_for_all)
+print("Jakość: ", fitness_for_all)
+print()
 
 
 """
 Test metody do selekcji turniejowej
 """
-def tournament_selection(population, fitness_scores, tournament_size = 2, selection_size = 3):
+print("SELEKCJA TURNIEJOWA")
+def tournament_selection(population, fitness_scores, tournament_size = 2, selection_size = 7):
     selected_indices = []
     for i in range(selection_size):
         tournament_indices = random.sample(range(len(population)), tournament_size)
@@ -59,8 +66,9 @@ def tournament_selection(population, fitness_scores, tournament_size = 2, select
     selected_population = [population[i] for i in selected_indices]
     return selected_population
 
-population_TS = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 1, 0, 0]]
-fitness_scores_TS = [4, 4, 3]
+population_TS = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 1, 0, 0], [1, 1, 0, 1], [1, 0, 0, 1], [0, 0, 0, 1], [1, 1, 1, 0]]
+fitness_scores_TS = [0, 0, -1, -5, -1, 1, -5]
 TS = tournament_selection(population_TS,fitness_scores_TS)
-print(TS)
+print("Populacja przed: ", population)
+print("Populacja po: ", TS)
 
