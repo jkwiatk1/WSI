43e427c25e5479d44b5e162ee01c3e517f846fc5
jkwiatk1 <01142696@pw.edu.pl>
2023-03-24 09:40:22 +0100

    Modyfikacja testow, dodanie grafów (przeniesienie testów z pliku experimetns)

--- Lab2/src/tests.py
+++ Lab2/src/tests.py
@@ -9,57 +9,59 @@ edges = [(0, 2), (0, 4), (0, 7), (1, 3), (1, 5), (1, 7), (2, 4), (2, 5), (3, 6),
 nodes = [0,1,2,3,4,5,6,7]
 number_of_all_edges = len(edges)
 
+"""
+Graf testowy
+"""
 for i in range(5):
-    GeneticAlgorithmInit_Test = GeneticAlgorithm(edges=edges, nodes=nodes, num_generations=10,
-                                                 mutation_probability=0.01, population_size=3, tournament_size=2,
-                                                 selection_size=3, mutation_rate=0.01)
+    GeneticAlgorithmInit_Test = GeneticAlgorithm(edges=edges, nodes=nodes, num_generations=100,
+                                                 mutation_probability=0.01, population_size=8, tournament_size=2,
+                                                 selection_size=8, mutation_rate=0.01)
     best = GeneticAlgorithmInit_Test.genetic_algorithm()
     result = best[1]/number_of_all_edges
-    print(best[0])
     show_graph(edges, f"Graf testowy, Pokrycie={result}, Pokryte: {best[1]}, Wszystkie: {number_of_all_edges}", best[0])
-
-
 
 
 """
 Graf pełny
 """
-# complete_graph_1_nodes, complete_graph_1_edges  = create_complete_graph(25)
-# print(complete_graph_1_edges)
-# print(len(complete_graph_1_edges))
-#
-# GeneticAlgorithmInit_Complete = GeneticAlgorithm(edges= complete_graph_1_edges, nodes= complete_graph_1_nodes, num_generations = 100, mutation_probability = 0.01, population_size = 50, tournament_size= 2, selection_size = 50, mutation_rate = 0.01)
-# best = GeneticAlgorithmInit_Complete.genetic_algorithm()
-# show_graph(complete_graph_1_edges, "Graf pełny",best[0])
-# print(best)
-#
-# for i in range(4):
-#     best = GeneticAlgorithmInit_Complete.genetic_algorithm()
-#     show_graph(complete_graph_1_edges, "Graf pełny", best[0])
-#     print(best)
-#     print()
+complete_graph_1_nodes, complete_graph_1_edges  = create_complete_graph(10)
+number_of_all_edges_Complete = len(complete_graph_1_edges)
+
+for i in range(5):
+    GeneticAlgorithmInit_Complete = GeneticAlgorithm(edges=complete_graph_1_edges, nodes=complete_graph_1_nodes,
+                                                     num_generations=100, mutation_probability=0.01, population_size=15,
+                                                     tournament_size=2, selection_size=15, mutation_rate=0.01)
+    best = GeneticAlgorithmInit_Complete.genetic_algorithm()
+    result = best[1]/number_of_all_edges_Complete
+    show_graph(complete_graph_1_edges, f"Graf pełny, Pokrycie={result}", best[0])
 
 
 
 """
 Graf dwudzielny
 """
-# bipartite_graph_1_nodes, bipartite_graph_1_edges = create_bipartite_graph(4,3)
-# print(bipartite_graph_1_edges)
-# show_graph(bipartite_graph_1_edges, "Graf dwudzielny")
-# GeneticAlgorithmInit_Bipartite = GeneticAlgorithm(edges= complete_graph_1_edges, nodes= complete_graph_1_nodes, num_generations = 10, mutation_probability = 0.01, population_size = 50, tournament_size= 2, selection_size = 50, mutation_rate = 0.01)
-# best = GeneticAlgorithmInit_Bipartite.genetic_algorithm()
-# print(best)
+
+for i in range(5):
+    bipartite_graph_1_nodes, bipartite_graph_1_edges = create_bipartite_graph(5, 5)
+    number_of_all_edges_Bipartite = len(bipartite_graph_1_edges)
+    GeneticAlgorithmInit_Bipartite = GeneticAlgorithm(edges=bipartite_graph_1_edges, nodes=bipartite_graph_1_nodes,
+                                                     num_generations=100, mutation_probability=0.01, population_size=15,
+                                                     tournament_size=2, selection_size=15, mutation_rate=0.01)
+    best = GeneticAlgorithmInit_Bipartite.genetic_algorithm()
+    result = best[1]/number_of_all_edges_Bipartite
+    show_graph(bipartite_graph_1_edges, f"Graf dwudzielny, Pokrycie={result}", best[0])
+
 
 
 """
 Graf z połową krawędzi
 """
-# random_graph_1_nodes, random_graph_1_edges = create_random_graph(7,0.5)
-# print(random_graph_1_edges)
-# show_graph(random_graph_1_edges, "Graf z połową krawędzi")
-#  GeneticAlgorithmInit_Random = GeneticAlgorithm(edges=random_graph_1_edges, nodes=random_graph_1_nodes,
-#                                                      num_generations=100, mutation_probability=0.01, population_size=50,
-#                                                      tournament_size=2, selection_size=50, mutation_rate=0.01)
-# best_R = GeneticAlgorithmInit_Random .genetic_algorithm(num_generations = 1000, mutation_probability = 0.01)
-# print(best_R)
+for i in range(5):
+    random_graph_1_nodes, random_graph_1_edges = create_random_graph(10,0.5)
+    number_of_all_edges_Random = len(random_graph_1_edges)
+    GeneticAlgorithmInit_Random = GeneticAlgorithm(edges=random_graph_1_edges, nodes=random_graph_1_nodes,
+                                                   num_generations=100, mutation_probability=0.01, population_size=15,
+                                                   tournament_size=2, selection_size=15, mutation_rate=0.01)
+    best = GeneticAlgorithmInit_Random.genetic_algorithm()
+    result = best[1]/number_of_all_edges_Random
+    show_graph(random_graph_1_edges, f"Graf losowy, Pokrycie={result}", best[0])
