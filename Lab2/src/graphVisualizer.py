a2bd13a0acd20adececfa1a8385e50e791decfe8
jkwiatk1 <01142696@pw.edu.pl>
2023-03-24 01:51:19 +0100

    Modyfikacja, przy wyswietlaniu w zaleznosci od stanu wezla ma on różny kolor

--- Lab2/src/graphVisualizer.py
+++ Lab2/src/graphVisualizer.py
@@ -1,17 +1,19 @@
+# author: Jan Kwiatkowski
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def visualize_graph(graph, title):
-    """
-    Wizualizacja grafu
-    """
+def visualize_graph(graph, title, values):
+    colors = ['green' if val == 1 else 'blue' for val in values]
 
     pos = nx.spring_layout(graph)
     plt.title(title)
-    nx.draw(graph, pos, with_labels=True, edge_color = "Black", node_color = "Red")
+    nx.draw(graph, pos, with_labels=True, edge_color = "Black", node_color = colors)
+    nx.draw_networkx_edges(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
     plt.show()
 
-def show_graph(edges, title):
+def show_graph(edges, title, values):
     graph = nx.Graph()
     graph.add_edges_from(edges)
-    visualize_graph(graph, title)
+    visualize_graph(graph, title, values)
