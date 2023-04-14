3f853a40d9e0a7e81b3da7e30200a73d57375591
jkwiatk1 <01142696@pw.edu.pl>
2023-04-14 18:38:56 +0200

    Data preprocessing for ML algorithm

---
+++ Lab4/src/dataPreparation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def preprocess_dataset():
+    df = pd.read_csv("../database/car.data", header=None, delimiter=',')
+    df.columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+    df = df.sample(frac=1)
+    ydf = df[[df.columns[-1]]]
+
+    df.drop(df.columns[-1], axis=1, inplace=True)
+    xdf = df
+
+    # One hot encoding
+    xdf = pd.get_dummies(data=xdf, columns=['buying', 'maint', 'lug_boot', 'safety'])
+    xdf['doors'].replace('5more', '5', inplace=True)
+    xdf['persons'].replace('more', '5', inplace=True)
+    xdf = xdf.apply(pd.to_numeric)
+
+    le = LabelEncoder()
+    le.fit(ydf)
+    ydf = le.transform(ydf)
+
+    X,y = xdf.values,ydf
+
+    return X,y
+
