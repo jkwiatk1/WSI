d27b13335621aceadba6bf808a13255d19b8495a
jkwiatk1 <01142696@pw.edu.pl>
2023-04-15 21:22:02 +0200

    Move files to `model` folder. Add informationGain file to calculate entropy

---
+++ Lab4/model/randomForest.py
@@ -0,0 +1,11 @@
+# author: Jan Kwiatkowski
+
+import math
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+from informationGain import entropy
+from informationGain import information_gain
+
+
