6240400ba0031a4468c1adc74376adfc59b47cd7
jkwiatk1 <01142696@pw.edu.pl>
2023-04-16 23:48:52 +0200

    Add class Metric2 and tests. Similar functionality to Metric class.

--- Lab4/model/Metrics.py
+++ Lab4/model/Metrics.py
@@ -44,20 +44,20 @@ class Metrics:
         return entropy_S + result
 
 
-# class Metrics2:
-#     def entropy(self, y):
-#         _, counts = np.unique(y, return_counts=True)
-#         p = counts / len(y)
-#         return -np.sum(p * np.log2(p))
-#
-#     def conditional_entropy(self, x, y):
-#         res = 0
-#         vals, counts = np.unique(x, return_counts=True)
-#         for val, count in zip(vals, counts):
-#             res += count / len(x) * self.entropy(y[x == val])
-#         return res
-#
-#     def information_gain(self, x, y, feature_idx):
-#         H_S = self.entropy(y)
-#         H_S_X = self.conditional_entropy(x[:, feature_idx], y)
-#         return H_S - H_S_X
+class Metrics2:
+    def entropy(self, y):
+        _, counts = np.unique(y, return_counts=True)
+        p = counts / len(y)
+        return -np.sum(p * np.log2(p))
+
+    def conditional_entropy(self, x, y):
+        res = 0
+        vals, counts = np.unique(x, return_counts=True)
+        for val, count in zip(vals, counts):
+            res += count / len(x) * self.entropy(y[x == val])
+        return res
+
+    def information_gain(self, x, y, feature_idx):
+        H_S = self.entropy(y)
+        H_S_X = self.conditional_entropy(x[:, feature_idx], y)
+        return H_S - H_S_X
--- Lab4/tests/tests.py
+++ Lab4/tests/tests.py
@@ -3,10 +3,11 @@ import unittest
 import numpy as np
 
 from model.dataPreparation import preprocess_dataset
-from model.Metrics import Metrics
+from model.Metrics import Metrics, Metrics2
 
 X, y = preprocess_dataset()
 metrics_test = Metrics()
+metrics_test2 = Metrics2()
 
 
 class Test(unittest.TestCase):
@@ -57,6 +58,25 @@ class Test(unittest.TestCase):
         a_idx = 0
         entropy_S = metrics_test.entropy(Sy)
         rounded_value = round(metrics_test.information_gain(Sx, Sy, a_idx, entropy_S), 2)
+        self.assertEquals(rounded_value, 0.13)
+
+
+    def test_entropyV2(self):
+        dataSet = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+        self.assertEquals(metrics_test2.entropy(dataSet), 1)
+
+    def test_entropyV2_2(self):
+        dataSet = [0, 0, 0, 0, 0, 0, 1, 1]
+        rounded_value = round(metrics_test2.entropy(dataSet), 2)
+        self.assertEquals(rounded_value, 0.81)
+
+    def test_InfGainV2(self):
+        Sx = np.array(
+            [[1, 1], [2, 2], [2, 2], [2, 1], [2, 2], [1, 2], [2, 1], [2, 1], [1, 2], [2, 2], [1, 2], [1, 2], [1, 2],
+             [2, 2]])
+        Sy = np.array([0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0])
+        a_idx = 0
+        rounded_value = round(metrics_test2.information_gain(Sx, Sy, a_idx), 2)
         self.assertEquals(rounded_value, 0.13)
 
 
