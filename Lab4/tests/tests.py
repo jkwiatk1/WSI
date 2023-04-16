d48a7797d91d0cc778d476720abfc328a5a491dc
jkwiatk1 <01142696@pw.edu.pl>
2023-04-16 23:14:48 +0200

    Add third InfGain test

--- Lab4/tests/tests.py
+++ Lab4/tests/tests.py
@@ -38,6 +38,7 @@ class Test(unittest.TestCase):
         rounded_value = round(metrics_test.information_gain(Sx, Sy, a_idx, entropy_S), 2)
         self.assertEquals(rounded_value, 0.05)
 
+
     def test_InfGain2(self):
         Sx = np.array(
             [[1, 1], [2, 2], [2, 2], [2, 1], [2, 2], [1, 2], [2, 1], [2, 1], [1, 2], [2, 2], [1, 2], [1, 2], [1, 2],
@@ -47,6 +48,16 @@ class Test(unittest.TestCase):
         entropy_S = metrics_test.entropy(Sy)
         rounded_value = round(metrics_test.information_gain(Sx, Sy, a_idx, entropy_S), 2)
         self.assertEquals(rounded_value, 0.04)
+
+    def test_InfGain3(self):
+        Sx = np.array(
+            [[1, 1], [2, 2], [2, 2], [2, 1], [2, 2], [1, 2], [2, 1], [2, 1], [1, 2], [2, 2], [1, 2], [1, 2], [1, 2],
+             [2, 2]])
+        Sy = np.array([0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0])
+        a_idx = 0
+        entropy_S = metrics_test.entropy(Sy)
+        rounded_value = round(metrics_test.information_gain(Sx, Sy, a_idx, entropy_S), 2)
+        self.assertEquals(rounded_value, 0.13)
 
 
 if __name__ == '__main__':
