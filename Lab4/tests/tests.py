3e710fc44e30a3de60379e477c55b9822409adcb
jkwiatk1 <01142696@pw.edu.pl>
2023-04-16 22:54:01 +0200

    Add informationGain test

--- Lab4/tests/tests.py
+++ Lab4/tests/tests.py
@@ -1,5 +1,6 @@
 # author: Jan Kwiatkowski
 import unittest
+import numpy as np
 
 from model.dataPreparation import preprocess_dataset
 from model.informationGain import Metrics
@@ -28,6 +29,16 @@ class Test(unittest.TestCase):
         dataSet = [0,0,0,0,1,1]
         rounded_value = round(metrics_test.entropy(dataSet),2)
         self.assertEquals(rounded_value, 0.92)
+
+    def test_InfGain(self):
+        Sx = np.array([[1, 1], [1, 2], [1, 3], [2, 1], [2, 2],[1,1],[1,2],[1,3],[2,2],[2,3]])
+        Sy = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+        a_idx = 1
+        entropy_S = metrics_test.entropy(Sy)
+        rounded_value = round(metrics_test.information_gain(Sx, Sy, a_idx, entropy_S),2)
+        self.assertEquals(rounded_value, 0.05)
+
+
 
 
 if __name__ == '__main__':
