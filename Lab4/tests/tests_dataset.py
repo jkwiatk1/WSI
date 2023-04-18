09b2b2495cad92f1dc985d6375b81934efc2f4ba
jkwiatk1 <01142696@pw.edu.pl>
2023-04-18 16:26:05 +0200

    Add test to check is X (atr) and y (classes) are the same size after processing.

--- Lab4/tests/tests_dataset.py
+++ Lab4/tests/tests_dataset.py
@@ -1,4 +1,5 @@
 # author: Jan Kwiatkowski
+import unittest
 
 from model.dataPreparation import preprocess_dataset
 
@@ -7,3 +8,7 @@ print("Atrybuty")
 print(X)
 print("Klasy")
 print(y)
+
+class Test(unittest.TestCase):
+    def test_dataset_size(self):
+        self.assertEquals(len(X), len(y))
