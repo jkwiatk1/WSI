895bbd39a6233dfaa72e57d77f5fd531dd671492
jkwiatk1 <01142696@pw.edu.pl>
2023-04-21 17:47:28 +0200

    Add option to sorted dataset by last parameter (class value)

--- Lab4/model/dataPreparation.py
+++ Lab4/model/dataPreparation.py
@@ -2,9 +2,13 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
-def preprocess_dataset(is_data_shuffle = False):
+def preprocess_dataset(is_data_shuffle = False,sort_by_last_column = False):
     df = pd.read_csv("../database/car.data", header=None, delimiter=',')
     df.columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+
+    if sort_by_last_column:
+        df = df.sort_values(df.columns[-1])
+
     if is_data_shuffle == True:
         df = df.sample(frac=1)
     ydf = df[[df.columns[-1]]]
